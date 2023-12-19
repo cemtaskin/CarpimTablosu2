20d687a4c9b89c843e26d826daae0c6693582d38
CEM TASKIN <cemtaskin82@gmail.com>
2023-12-19 11:42:54 +0300

    Initial Commit

---
+++ CarpimTablosu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarpimTablosu
+//
+//  Created by Cem TAŞKIN on 19.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
